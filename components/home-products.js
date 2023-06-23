018ef10415bd603081bc700531042a65047743e7
Parag Chauhan <parag.chauhan@bacancy.com>
2023-06-23 13:41:09 +0530

    website anchor link changes

--- components/home-products.js
+++ components/home-products.js
@@ -10,7 +10,7 @@ const HomeProducts = () => {
           <div className="home_products__detail">
             <h2><span>PillarDAO</span> Products</h2>
             <div className="home_products__detail__list">
-              <Link href="https://www.pillar.fi/?utm_source=plrstakinglp&utm_medium=website&utm_campaign=plr_staking" target="_blank" rel="noopener noreferrer">
+              <Link href="https://www.pillar.fi/?utm_source=pillar_website&utm_medium=page&utm_campaign=pillar_dao" target="_blank" rel="noopener noreferrer">
                 <img src={logoPillar} alt="Pillar" title="Pillar" />
               </Link>
             </div>
