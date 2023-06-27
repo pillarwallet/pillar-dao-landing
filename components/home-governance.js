eaf74cc6d92be24742f0b3ec7978eaf07983951e
Parag Chauhan <parag.chauhan@bacancy.com>
2023-06-27 10:23:31 +0530

    fix styling for PLR DAO block list

--- components/home-governance.js
+++ components/home-governance.js
@@ -27,13 +27,15 @@ const HomeGovernance = () => {
             <h2>How to become a <span>Governor</span></h2>
             <div className="home_governance__app__detail_wrapper">
               <div className="home_governance__app__detail">
-                <p>In order to take part in the governance of PillarDAO, Members are required to lock-in 10,000 PLR tokens into the Polygon smart contract on the membership dApp below.</p>
-                <WagmiProvider>
-                  <PlrDaoStakingBuilder />
-                </WagmiProvider>
+                <p className="home_governance__app__detail_paragraph">In order to take part in the governance of PillarDAO, Members are required to lock-in 10,000 PLR tokens into the Polygon smart contract on the membership dApp below.</p>
+                <div className='plr_dao_block' >
+                  <WagmiProvider>
+                    <PlrDaoStakingBuilder />
+                  </WagmiProvider>
+                </div>
               </div>
               <div className="home_governance__app__detail">
-                <p>Once locked each member will receive a membership NFT signalling their governor status and&nbsp;used for verification.</p>
+                <p className="home_governance__app__detail_paragraph">Once locked each member will receive a membership NFT signalling their governor status and&nbsp;used for verification.</p>
                 <img className="dao_member_img" src={governanceNFT} alt="" />
                 <ul>
                   <li>The lock-in period is for 52 weeks after which a member can optionally burn their membership NFT and unlock their PLR tokens, deactivating governor status.</li>
