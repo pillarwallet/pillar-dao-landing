28a37473247b5064c45415870bb35a4b841cc071
Parag Chauhan <parag.chauhan@bacancy.com>
2023-08-17 13:44:05 +0530

    added google tag manager script

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+import Script from 'next/script';
 
 const Layout = ({ pageTitle, children }) => {
   return (
@@ -23,6 +24,17 @@ const Layout = ({ pageTitle, children }) => {
         <meta name="twitter:title" content={pageTitle} />
         <meta name="twitter:description" content="PillarDAO builds, funds and explores web3, multichain infrastructure and technology for a decentralised future. Join us now!" />
         <meta name="twitter:image" content="https://pillardao.org/pillar_dao.png" />
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-WHWT189N5K"></Script>
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `if(typeof window !== "undefined"){
+              window.dataLayer = window.dataLayer || [];
+             function gtag(){dataLayer.push(arguments)}
+              gtag('js', new Date());
+              gtag('config', 'G-WHWT189N5K');
+            }`
+          }}
+        />
       </Head>
       <div className="page-wrapper" id="wrapper">
         {children}
