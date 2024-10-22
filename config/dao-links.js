d190b31f79fc0e8353cbb938319c87920d319753
zbcoding <zbcoding@outlook.com>
2024-10-22 16:57:59 -0500

    Create dao-links file for variable source of dao vote and social links

---
+++ config/dao-links.js
@@ -0,0 +1,7 @@
+export const daoLinks = {
+    DAO_BLOG: `https://pillardao.substack.com`,
+    DAO_VOTE_PLATFORM: `https://snapshot.org/#/plrdao.eth`,
+    DAO_SOCIAL_DISCORD: 'https://discord.gg/t39x',
+    DAO_SOCIAL_TWITTER_X: `https://x.com/pillar_dao`,
+    DAO_SOCIAL_GUILDXYZ: `https://guild.xyz/pillardao-official`,
+  }
