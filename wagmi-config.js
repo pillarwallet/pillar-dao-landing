2fd29a2b844c00ab788572b942c277ee0b02a5da
zbcoding <zbcoding>
2024-10-25 08:10:13 -0500

    Remove unused chains, fix blockchains in config

--- wagmi-config.js
+++ wagmi-config.js
@@ -7,14 +7,8 @@ import {
   optimism,
   avalanche,
   celo,
-  // gnosis,
-  // okc,
-  // moonbeam,
-  // fantom,
-  // aurora,
 } from 'wagmi/chains';
 import { metaMask, walletConnect, coinbaseWallet, safe } from 'wagmi/connectors';
-// import pillarDaoLogo from '../assets/images/pillar-dao-logo.png';
 import pillarDaoIcon from '@images/pillar-dao-logo-purple-48px.png';
 
 const blockchains = [
@@ -25,16 +19,11 @@ const blockchains = [
   optimism,
   avalanche,
   celo,
-  // gnosis,
-  // okc,
-  // moonbeam,
-  // fantom,
-  // aurora,
 ];
-const INFURA_KEY = process.env.NEXT_PUBLIC_INFURA_API_KEY;
+const INFURA_ID = process.env.NEXT_PUBLIC_INFURA_API_KEY;
 
 export const wagmiConfig = createConfig({
-  chains: [mainnet],
+  chains: [...blockchains],
   ssr: true, //server-side rendering
   connectors: [
     metaMask({
@@ -60,17 +49,12 @@ export const wagmiConfig = createConfig({
     safe(),
   ],
   transports: {
-    [mainnet.id]: http(`https://mainnet.infura.io/v3/${INFURA_KEY}`),
-    [polygon.id]: http(`https://polygon-mainnet.infura.io/v3/${INFURA_KEY}`),
-    [arbitrum.id]: http(`https://arbitrum-mainnet.infura.io/v3/${INFURA_KEY}`),
-    [bsc.id]: http(`https://bsc-mainnet.infura.io/v3/${INFURA_KEY}`),
-    [optimism.id]: http(`https://optimism-mainnet.infura.io/v3/${INFURA_KEY}`),
-    [avalanche.id]: http(`https://avalanche-mainnet.infura.io/v3/${INFURA_KEY}`),
-    [celo.id]: http(`https://celo-mainnet.infura.io/v3/${INFURA_KEY}`),
-    // [okc.id]: http(`https://mainnet.infura.io/v3/${INFURA_KEY}`),
-    // [moonbeam.id]: http(`https://mainnet.infura.io/v3/${INFURA_KEY}`),
-    // [fantom.id]: http(`https://mainnet.infura.io/v3/${INFURA_KEY}`),
-    // [aurora.id]: http(`https://mainnet.infura.io/v3/${INFURA_KEY}`),
-    // [gnosis.id]: http(`https://mainnet.infura.io/v3/${INFURA_KEY}`),
+    [mainnet.id]: http(`https://mainnet.infura.io/v3/${INFURA_ID}`),
+    [polygon.id]: http(`https://polygon-mainnet.infura.io/v3/${INFURA_ID}`),
+    [arbitrum.id]: http(`https://arbitrum-mainnet.infura.io/v3/${INFURA_ID}`),
+    [bsc.id]: http(`https://bsc-mainnet.infura.io/v3/${INFURA_ID}`),
+    [optimism.id]: http(`https://optimism-mainnet.infura.io/v3/${INFURA_ID}`),
+    [avalanche.id]: http(`https://avalanche-mainnet.infura.io/v3/${INFURA_ID}`),
+    [celo.id]: http(`https://celo-mainnet.infura.io/v3/${INFURA_ID}`),
   },
 });
