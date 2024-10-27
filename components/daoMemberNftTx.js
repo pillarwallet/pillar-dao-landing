b67c4ce614d988390204ba0a5782c3e3c2735347
zbcoding <zbcoding>
2024-10-27 02:31:36 -0500

    Use polygon mainnet contracts instead of Amoy testnet

--- components/daoMemberNftTx.js
+++ components/daoMemberNftTx.js
@@ -15,16 +15,16 @@ Polygon PLR token: 0xa6b37fC85d870711C56FbcB8afe2f8dB049AE774
 Dao contract: 0xc380f15Db7be87441d0723F19fBb440AEaa734aB
 
 ---Polygon Amoy test contracts---
-test nft: 0x0901f5aBd34A9080Dded6dad72188aAbee8a976F
-test tokens (airdrop): 0x3cb29AAC77693A0784380Fb664Ec443Ce1079882
 test dao contract: 0xf1a8685519D456f47a9F3505035F4Bad5d9a9ce0
+test tokens (airdrop): 0x3cb29AAC77693A0784380Fb664Ec443Ce1079882
 required test stake amount: 10*10**18 = 10000000000000000000 (10 tokens)
 required test stake time: 0.1 min (6 seconds)
+test nft: 0x0901f5aBd34A9080Dded6dad72188aAbee8a976F
 */
 
-const polygonChainId = 80002; //80002 amoy testnet, 137 polygon mainnet
-const daoContractAddress = '0xf1a8685519d456f47a9f3505035f4bad5d9a9ce0';
-const tokenAddress = '0x3cb29aac77693a0784380fb664ec443ce1079882';
+const polygonChainId = 137; //80002 amoy testnet, 137 polygon mainnet
+const daoContractAddress = '0xc380f15Db7be87441d0723F19fBb440AEaa734aB';
+const tokenAddress = '0xa6b37fC85d870711C56FbcB8afe2f8dB049AE774';
 const stakeTokenAmount = ethers.utils.parseUnits('10', 18);
 const explorer = `https://polygonscan.com/tx/`;
 
